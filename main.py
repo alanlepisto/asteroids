7631229df48c0b4031e190c9a8db7f074d45d37a
alanlepisto <alan@lepisto.dev>
2026-02-17 21:52:11 -0600

    player finished

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -10,8 +11,19 @@ def main():
     print(f" Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
+        ptime = clock.tick(60)
+        dt = ptime / 1000
+        updatable.update(dt)
         log_state()
 
         for event in pygame.event.get():
@@ -19,6 +31,8 @@ def main():
                 return
 
         screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
 
 
